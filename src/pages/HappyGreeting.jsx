a61794a13c538537fefa697777dac0a78366c2e3
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2024-09-24 21:27:57 -0600

    Super many changes

--- src/pages/HappyGreeting.jsx
+++ src/pages/HappyGreeting.jsx
@@ -14,7 +14,9 @@ import {
   clamp,
 } from 'framer-motion';
 
-//happy monthsary, anniversary
+import { sleep } from '../utils/HelperFunctions';
+import { Await } from 'react-router-dom';
+
 export default function HappyGreeting(date) {
   // if date is nov 19, anniverasary
   // if date is 19, monthsary
@@ -28,8 +30,14 @@ export default function HappyGreeting(date) {
       'Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet ',
   };
 
+  // * music
+
+  let introBeats = new Audio('/music/IntroBeats.mp3');
+  let fullSong = new Audio('/music/FullSong.mp3');
+
   const seqeunceRef = useRef({});
-  const [activeSequence, setActiveSequence] = useState(4);
+  // todo manually control page here
+  const [activeSequence, setActiveSequence] = useState(1);
 
   let pawsVariant = {
     hidden: {
@@ -40,7 +48,7 @@ export default function HappyGreeting(date) {
       transition: {
         when: 'beforeChildren',
         duration: 0.5,
-        staggerChildren: 0.1,
+        staggerChildren: 0.5,
         staggerDirection: -1,
       },
     },
@@ -56,6 +64,10 @@ export default function HappyGreeting(date) {
   };
 
   useEffect(() => {
+    introBeats.loop = true;
+    introBeats.muted = 'muted';
+
+    introBeats.autoplay = true;
     function set(num, ms) {
       let timeout = setTimeout(() => {
         setActiveSequence(num);
@@ -64,8 +76,8 @@ export default function HappyGreeting(date) {
       return timeout;
     }
     // todo
-    // let secondSeq = set(2, 10000);
-    // let thirdSeq = set(3, 15000);
+    let secondSeq = set(2, 10000);
+    let thirdSeq = set(3, 15000);
   }, []);
 
   // * 3RD SEQUENCE, SNAPPING HEART
@@ -91,9 +103,11 @@ export default function HappyGreeting(date) {
       console.log(heartPos.current);
 
       if (distance < SNAP_RADIUS) {
+        introBeats.pause();
         enableDrag.set(false);
         await animate(HeartRef.current, { x: 0, y: snapCenter.y, translateY: '-50%' }, { duration: 1 });
-        await animate(HeartRef.current, { scale: 100 }, { ease: 'easeInOut', duration: 2 });
+        fullSong.play();
+        await animate(HeartRef.current, { scale: 100 }, { ease: 'easeInOut', duration: 3, delay: 0.5 });
         await animate(SnappingHeartRef.current, { display: 'none' });
 
         setActiveSequence(4);
@@ -176,33 +190,28 @@ export default function HappyGreeting(date) {
               animate="show"
             >
               <Paw
-                className={'mr-[20%]'}
+                className={'paws-left mr-[20%]'}
                 variants={pawItemsVariant}
-                delay={1.25}
               />
               <Paw
-                className={'mr-[-20%]'}
+                className={'paws-right mr-[-20%]'}
                 variants={pawItemsVariant}
-                delay={1}
               />
               <Paw
-                className={'mr-[20%]'}
+                className={'paws-left mr-[20%]'}
                 variants={pawItemsVariant}
-                delay={0.75}
               />
 
               <Paw
-                className={'mr-[-20%]'}
+                className={'paws-right mr-[-20%]'}
                 variants={pawItemsVariant}
-                delay={0.5}
               />
               <Paw
-                className={'mr-[20%]'}
+                className={'paws-left mr-[20%]'}
                 variants={pawItemsVariant}
-                delay={0.25}
               />
               <Paw
-                className={'mr-[-20%]'}
+                className={'paws-right mr-[-20%]'}
                 variants={pawItemsVariant}
               />
             </motion.div>
@@ -248,7 +257,7 @@ export default function HappyGreeting(date) {
                   exit={{ opacity: 0, position: 'fixed' }}
                 />
 
-                {snapCenter.x ? (
+                {/*  {snapCenter.x ? (
                   <div
                     className="absolute h-1 w-1 bg-pink-50"
                     style={{
@@ -256,7 +265,7 @@ export default function HappyGreeting(date) {
                       top: `${snapCenter.y}px`,
                     }}
                   ></div>
-                ) : null}
+                ) : null} */}
               </motion.div>
               <h2 className="mx-auto mt-auto pb-4 font-serif tracking-wider text-pink-50">{BirthdayMessage.third}</h2>
             </motion.div>
@@ -291,87 +300,154 @@ function LongMessagePage() {
         key="messages"
       >
         <section className="snap-start scroll-mb-4 scroll-mt-4">
-          <p className="mb-8 min-h-dvh px-4 font-serif leading-[170%] tracking-wide text-pink-50">
-            Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-            tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit
-            sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac
-            scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel
-            bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Forem ipsum
-            dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed
-            dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.
-            Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent
-            per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante
-            pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem.
-            Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Forem ipsum dolor sit amet,
-            consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-            fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget
-            condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia
-            nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.
-            Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi
-            convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Forem ipsum dolor sit amet,
-            consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-            fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget
-            condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia
-            nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.
-            Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi
-            convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Forem ipsum dolor sit amet,
-            consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-            fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget
-            condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia
-            nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.
-            Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi
-            convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
-          </p>
+          <div>
+            <p className="mb-8 px-4 font-serif leading-[170%] tracking-wide text-pink-50">
+              Hi love, it’s your birthday! Wow sah. Dali ra kaayu ang panahon nuh? It’s so crazy how time moved so fast,
+              so many things happened already, and mountain of life events that we forgot how we got to this point given
+              the endless pointy-ass needles life have had been poking me. But one thing is for sure, that you made this
+              life a heaven of a ride. That I was sure that I was not alone in facing OP boss battles. And through it
+              all, even in my most rainiest days, you were always there holding an umbrella for me, shielding me from
+              every raindrop.
+            </p>
+
+            <p className="mb-8 px-4 font-serif leading-[170%] tracking-wide text-pink-50">
+              You unlocked an unchartered territory of my heart. You taught me what true love means. You do
+              unexplainable things that make my heart pound like they be running on gasoline. All of these sa isa rana
+              ka tuig. Goodness gracious, jusko santa maria inahan ka sa Diyos. What did I do to deserve you?
+            </p>
+
+            <p className="mb-8 px-4 font-serif leading-[170%] tracking-wide text-pink-50">
+              I’ve witnessed your birthday twice now, and our love has grown thrice as much. Wala gyud damha sah nga mag
+              kita. Pero as they say nga, “You will find love when you least expect it”. Damn, I didn’t expect to find
+              it with you. I mean, naa diay tong meant to be nako gapuyo sa suburban part sa Lacion. Pai, all my life
+              this heart has been waiting for you. Only you. You don’t know how lucky I am for you. (Also, you don’t
+              know how beautiful and sexy and cutie patootie Papai you are.) I love you a gazillion times! My only wish
+              for you is for your happiness kay seeing you happy is also my happiness. I wish you all the best, my love.
+            </p>
+          </div>
         </section>
 
-        <PictureMessage key={1} />
-        <PictureMessage key={2} />
-        <PictureMessage key={3} />
+        <PictureMessage
+          key={1}
+          imgSrc={'/images/bite.jpg'}
+          header={'Bite that kugmo factory!'}
+          p={'Since I can’t squish you, I must paak you. Pakyu! That’s for being so cute uwu'}
+        />
+
+        <PictureMessage
+          key={2}
+          imgSrc={'/images/kingking.jpg'}
+          header={'First meetup with a fam'}
+          p={'Wow ah. Ana pa’s Kingking ganahan siya nimo because you listen. True. You are a level 9999 listener.'}
+        />
+
+        <PictureMessage
+          key={3}
+          imgSrc={'/images/foods.jpg'}
+          header={'First pamisita'}
+          p={
+            'Wow, food! Ofcors apil jud nang favorite memory ang foods. Matmat enjoyed the mini donuts so much, he so cute T__T  I lab food, but tati is d food that I labber. '
+          }
+        />
+
+        <PictureMessage
+          key={4}
+          imgSrc={'/images/foreshadow.jpg'}
+          header={'Foreshadowing'}
+          p={'Ay maypa foreshadowing diay ta nila? We so cool!'}
+        />
+
+        <PictureMessage
+          key={5}
+          imgSrc={'/images/movie-date.jpg'}
+          header={'First movie date'}
+          p={'First movie date nga nauwian sa first kiss! Wahhahah kilig namarn'}
+        />
+
+        <PictureMessage
+          key={6}
+          imgSrc={'/images/olango.jpg'}
+          header={'Island sunsets with you'}
+          p={
+            'I could live in this moment forever. Huhuhu I miss you so much. I could be stuck in an island with you and I’d probably be fine (moangal nis Janice ani ron kay wa mi kan-on)'
+          }
+        />
+
+        <PictureMessage
+          key={7}
+          imgSrc={'/images/valentines.jpg'}
+          header={'Valentine’s dateee'}
+          p={'Aweee first valentine’s date ever with my favorite person!'}
+        />
+
+        <PictureMessage
+          key={8}
+          imgSrc={'/images/video-call.jpg'}
+          header={'Video calls'}
+          p={
+            'This is taking the top spot as this is how we spend most of our time together. I love talking with you bb, I’m not that talkative to everyone baya even sa akong mga ka work ari but to you I am. I am soaper comfy with you. You are my comfort <3 Ang 2nd image kay kato nang ga video call ta. Abi nakog natog naka pero gisapot man diay ka nako hehe. Labyuuu'
+          }
+        />
 
         <section className="-m-6 grid h-dvh snap-center grid-cols-[repeat(2,_minmax(0px,_200px))] justify-around overflow-hidden font-serif text-pink-50">
           {/* moviehaus */}
           <div className="relative m-auto h-full w-full">
-            <motion.img
-              className="polka-img moviehaus absolute top-1/2 w-[50vw] -translate-x-1/4 -translate-y-1/2 rotate-45 scale-125"
+            <JigglingPics
+              className="absolute top-1/2 w-[50vw] -translate-x-1/4 -translate-y-1/2 scale-125"
               src="/polkadot_pics/moviehaus.png"
               alt="moviehaus"
+              initialClassname="polka-img-grow"
+              finalClassname="polka-img moviehaus"
             />
           </div>
 
-          {/* park */}
-          <img
-            className="polka-img-grow polka-img park m-auto"
+          {/* park
+          className, src, alt, initialClassname, finalClassname */}
+
+          <JigglingPics
+            className="m-auto"
             src="/polkadot_pics/park.png"
             alt="park"
+            initialClassname="polka-img-grow"
+            finalClassname="polka-img park"
           />
 
           <div className="col-span-2 content-center text-center">
-            <p>I love you to the core.</p>
+            <p>
+              Cheers for more love, movie dates, ranked games, birthdays and Spotify monthly subscriptions to come. I
+              love you. I miss you so much.
+            </p>
             <p>Happy birthday, my love!</p>
           </div>
 
           {/* olango */}
-          <img
-            className="polka-img olango"
+
+          <JigglingPics
+            className=""
             src="/polkadot_pics/olango.png"
             alt="olango"
+            initialClassname="polka-img-grow"
+            finalClassname="polka-img olango"
           />
 
           {/* elevator */}
           <div className="relative row-span-2 m-auto h-full w-full">
-            <img
-              className="polka-img elevator absolute top-1/2 -rotate-[30deg]"
+            <JigglingPics
+              className="absolute top-1/2 -rotate-[30deg]"
               src="/polkadot_pics/elevator.png"
               alt="elevator"
+              initialClassname="polka-img-grow"
+              finalClassname="polka-img elevator"
             />
           </div>
 
           {/* sm */}
-          <img
-            className="polka-img sm mt-auto"
+          <JigglingPics
+            className="mt-auto"
             src="/polkadot_pics/sm.png"
             alt="sm"
+            initialClassname="polka-img-grow"
+            finalClassname="polka-img sm"
           />
         </section>
       </motion.div>
@@ -379,7 +455,41 @@ function LongMessagePage() {
   );
 }
 
-function PictureMessage(props) {
+function JigglingPics({ className, src, alt, initialClassname, finalClassname }) {
+  return (
+    <motion.img
+      className={`${className} ${initialClassname}`}
+      src={src}
+      alt={alt}
+      onViewportEnter={async (info) => {
+        info.target.classList.remove(initialClassname);
+        info.target.classList.add(...finalClassname.split(' '));
+      }}
+      onViewportLeave={async (info) => {
+        await sleep(200);
+        // Remove all final classes and re-add the initial class
+        info.target.classList.remove(...finalClassname.split(' '));
+        await sleep(200);
+        info.target.classList.add(initialClassname);
+      }}
+      viewport={{ once: true }}
+    />
+  );
+}
+
+function usePreloadImage(src) {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => setLoaded(true);
+  }, [src]);
+
+  return loaded;
+}
+
+function PictureMessage({ header, imgSrc, p }) {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -387,28 +497,24 @@ function PictureMessage(props) {
   });
 
   const scaleImg = useTransform(scrollYProgress, [0, 1], [0.5, 1], { clamp: false });
-  const scale = useSpring(scaleImg, { stiffness: 300, damping: 30 });
+  const scale = useSpring(scaleImg, { stiffness: 150, damping: 20 });
+  const isLoaded = usePreloadImage(imgSrc); // Preloading image
 
-  /*   scrollYProgress.on('change', () => {
-    console.log(scrollYProgress.current);
-  }); */
+  // if (!isLoaded) return null; // or display a loader
 
   return (
     <motion.section
       className="flex h-screen snap-center flex-col items-center justify-center gap-6"
-      style={{ scale: scale, opacity: scale }}
+      style={{ scale: scale }}
       ref={ref}
     >
-      <h1 className="font-cooper text-[7vw] text-pink-50 sm:text-4xl">Island sunset with you</h1>
-      <motion.img
-        src="/olango.png"
-        alt="Olango sunset"
+      <h1 className="font-cooper text-[7vw] text-pink-50 sm:text-4xl">{header}</h1>
+      <img
+        src={imgSrc}
+        alt={imgSrc}
         className="rounded-3xl"
       />
-      <p className="font-serif leading-[170%] tracking-wide text-pink-50">
-        Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet
-        odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-      </p>
+      <p className="font-serif leading-[170%] tracking-wide text-pink-50">{p}</p>
     </motion.section>
   );
 }
